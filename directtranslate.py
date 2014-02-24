8d436348d54fb1dffa9ae00ce5edccce6c2f2d63
Jim Reesman <jimreesman@yahoo.com>
2014-02-24 15:47:58 -0800

    split sentence.txt into -dev and -test

--- directtranslate.py
+++ directtranslate.py
@@ -1,5 +1,6 @@
 
 import sys
+import getopt
 import random
 import re
 import copy
@@ -21,7 +22,7 @@ with open( 'dict2.txt', 'r' ) as df:
 			dict[ elements[0] ] = elements[1].split('#')[0].split(',')
 
 
-def wordorder(sentence, swahili):
+def wordorder(sentence, swahili, tokens):
 	#Transform V - N -ADJ to V-ADJ-N
 	K = len(sentence)
 	print K, "+++++++++++++++++TRANSFORMING#############",tokens
@@ -31,8 +32,8 @@ def wordorder(sentence, swahili):
 	temp = copy.deepcopy(sentence)
 	for i in range(K):
 		word = sentence[i]
+		j = i + 2
 		if word == '[N]':
-			j = i + 2
 			if j < K: #possible to have an adjective after noun
 				if sentence[i+2] == '[ADJ]':
 					temp[i-1] = sentence[i+1]
@@ -66,38 +67,56 @@ def wordorder(sentence, swahili):
 	return sentence		
 		
 # naive direct translation of sentence.txt
-with open('sentence.txt', 'r') as sf:
-	for line in sf:
-		print line
-		tokens = re.findall(r'\w+', line.lower())
-		print "TOKENS====",tokens
-		swahili = []
-		translated = []
-		for i in range(len(tokens) ):
-			word = tokens[i].lower()
-			if word in dict:
-				translations = dict[word]
-				k = len(translations)
-				i = random.randint(0, k-1)
-				if k > 1:
-					translated.append(translations[i])
+def translate(sentenceFile):
+	with open(sentenceFile, 'r') as sf:
+		for line in sf:
+			print line
+			tokens = re.findall(r'\w+', line.lower())
+			print "TOKENS====",tokens
+			swahili = []
+			translated = []
+			for i in range(len(tokens) ):
+				word = tokens[i].lower()
+				if word in dict:
+					translations = dict[word]
+					k = len(translations)
+					i = random.randint(0, k-1)
+					if k > 1:
+						translated.append(translations[i])
+					else:
+						translated.append( dict[word][0] )
+
+					translated.append("["+POSTAG[word]+"]")
+					
+					#swahili copy to use in stemmer
+					swahili.append([word])
+					swahili.append("["+POSTAG[word]+"]")
+
 				else:
-					translated.append( dict[word][0] )
+					translated.append(word)
+					translated.append("[N]")
 
-				translated.append("["+POSTAG[word]+"]")
-				
-				#swahili copy to use in stemmer
-				swahili.append([word])
-				swahili.append("["+POSTAG[word]+"]")
+					#swahili copy to use in stemmer
+					swahili.append(word)
+					swahili.append('[N]')
 
-			else:
-				translated.append(word)
-				translated.append("[N]")
+			print '+++++++++TRANSLATED=======',translated
+			reordered = wordorder(translated, swahili,tokens)
+			print ' '.join(reordered)
 
-				#swahili copy to use in stemmer
-				swahili.append(word)
-				swahili.append('[N]')
 
-		print '+++++++++TRANSLATED=======',translated
-		reordered = wordorder(translated, swahili)
-		print ' '.join(reordered)
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    file = 'sentence-dev.txt'
+    if len(argv) == 2:
+    	if argv[1] == 'test':
+    		file = 'sentence-test.txt'
+
+    translate(file)
+
+
+if __name__ == "__main__":
+    main()
